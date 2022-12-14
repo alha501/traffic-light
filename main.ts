44ed6b2a151af9ad899211707c1765a2672f58e5
alha501 <113909973+alha501@users.noreply.github.com>
2022-12-14 09:02:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,45 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
         YELLOW()
-        for (let index = 0; index < 3; index++) {
-            basic.pause(1000)
-        }
+        basic.pause(3000)
         RED()
     } else if (receivedNumber == 35) {
-        basic.pause(2000)
-        RED()
-        basic.pause(2000)
-        YELLOW()
-        for (let index = 0; index < 3; index++) {
-            basic.pause(1000)
-        }
+        basic.pause(3000)
         GREEN()
-        for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 3; index++) {
             basic.pause(5000)
         }
     }
 })
+function Sonar () {
+    for (let index = 0; index < 6; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    }
+    if (distance <= 5) {
+        GREEN()
+        counter += 1
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    resume_traffic_light()
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+    }
+    GREEN()
+    basic.showIcon(IconNames.StickFigure)
+    for (let index = 0; index <= 15; index++) {
+        basic.showNumber(15 - index)
+    }
+    basic.showIcon(IconNames.No)
+    YELLOW()
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+    }
+    RED()
 })
 function RED () {
     range = strip.range(0, 1)
@@ -31,15 +50,18 @@ function RED () {
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
 function resume_traffic_light () {
-    basic.pause(2000)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+    }
     YELLOW()
-    for (let index = 0; index < 3; index++) {
+    for (let index = 0; index < 4; index++) {
         basic.pause(1000)
     }
     GREEN()
     for (let index = 0; index < 3; index++) {
         basic.pause(5000)
     }
+    RED()
 }
 function GREEN () {
     range = strip.range(0, 1)
@@ -50,27 +72,26 @@ function GREEN () {
     range.showColor(neopixel.colors(NeoPixelColors.Green))
 }
 input.onButtonPressed(Button.B, function () {
-    resume_traffic_light()
+    music.setTempo(304)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+    }
+    GREEN()
     basic.showIcon(IconNames.StickFigure)
     music.playMelody("E D G F B A C5 B ", 196)
     for (let index = 0; index <= 15; index++) {
         basic.showNumber(15 - index)
-        music.playMelody("C5 A E D C - - - ", 196)
+        music.playMelody("C5 A E D C - - - ", 300)
+        music.changeTempoBy(200)
     }
-    music.playMelody("E D C C C C C C ", 196)
-    basic.pause(500)
-    resume_traffic_light()
+    music.playMelody("E D C C C C C C ", 300)
+    basic.showIcon(IconNames.No)
+    YELLOW()
+    for (let index = 0; index < 4; index++) {
+        basic.pause(1000)
+    }
     RED()
 })
-function sensor () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-    basic.pause(2000)
-}
 function YELLOW () {
     range = strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -79,22 +100,16 @@ function YELLOW () {
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-let counter = 0
 let range: neopixel.Strip = null
 let distance = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
 strip.setBrightness(70)
 RED()
-basic.showIcon(IconNames.No)
+basic.showIcon(IconNames.Yes)
 radio.setGroup(32)
-distance = 5
+distance = 7
+let counter = 0
 basic.forever(function () {
-    sensor()
-    if (distance < 5) {
-        counter = 1
-    }
-    if (counter == 4) {
-        resume_traffic_light()
-    }
+    Sonar()
 })
